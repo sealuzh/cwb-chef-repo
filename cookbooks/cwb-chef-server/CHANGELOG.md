bc4859fbd94720969c9b5b91ea6fecb49a4fe24c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-07-14 03:15:30 +0200

    Update cwb-chef-server

--- cookbooks/cwb-chef-server/CHANGELOG.md
+++ cookbooks/cwb-chef-server/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.0 (2017-07-14)
+
+* Bump Chef server dependency using Chef 13 now
+
 # 0.1.0 (2016-03-28)
 
 * Initial release of this wrapper cookbook for Chef Server 12.
--- cookbooks/cwb-chef-server/metadata.rb
+++ cookbooks/cwb-chef-server/metadata.rb
@@ -1,9 +1,9 @@
 name 'cwb-chef-server'
 maintainer 'Joel Scheuner'
 maintainer_email 'joel.scheuner.dev@gmail.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Wrapper cookbook for chef-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 
-depends 'chef-server', '~> 4.1.0'
+depends 'chef-server', '~> 5.4.0'
