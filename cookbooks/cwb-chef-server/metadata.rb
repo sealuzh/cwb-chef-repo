b45fd11269f329eea1bad31d8e56da172d7d5d47
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-14 14:55:35 +0200

    Update dependencies
    * Add `Berksfile.lock` to version control

--- cookbooks/cwb-chef-server/metadata.rb
+++ cookbooks/cwb-chef-server/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joel.scheuner.dev@gmail.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook for chef-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 
-depends 'chef-server', '~> 4.1.0'
+depends 'chef-server', '~> 5.5.2'
