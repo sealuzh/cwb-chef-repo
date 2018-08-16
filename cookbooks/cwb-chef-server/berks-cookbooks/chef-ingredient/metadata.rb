14748d2a5eb2cddaedf0ac5d3ac2c9886e2e3381
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-16 16:20:37 +0200

    Update vendored berks-cookbooks

---
+++ cookbooks/cwb-chef-server/berks-cookbooks/chef-ingredient/metadata.rb
@@ -0,0 +1,14 @@
+name 'chef-ingredient'
+version '2.3.2'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache-2.0'
+description 'Primitives for managing Chef products and packages'
+
+%w(amazon centos redhat scientific oracle fedora debian ubuntu).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/chef-ingredient'
+issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
+chef_version '>= 12.7' if respond_to?(:chef_version)
---
+++ cookbooks/cwb-chef-server/berks-cookbooks/cwb-chef-server/metadata.rb
@@ -0,0 +1,9 @@
+name 'cwb-chef-server'
+maintainer 'Joel Scheuner'
+maintainer_email 'joel.scheuner.dev@gmail.com'
+license 'Apache 2.0'
+description 'Wrapper cookbook for chef-server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.2.0'
+
+depends 'chef-server', '~> 5.5.2'
