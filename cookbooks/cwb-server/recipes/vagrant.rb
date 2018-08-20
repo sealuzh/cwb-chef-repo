1f9053aa7945ff825f797759cc5857aa07bdac1e
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-20 18:25:28 +0200

    Fix vagrant-butcher plugin install
    * Depending on the file cache path might not work in other environments. Using cookbook file instead.
    ```
    butcher_path = "#{Chef::Config[:file_cache_path]}/cookbooks/cwb-server/files/default/vagrant-butcher-#{version}.gem"
    ```

--- cookbooks/cwb-server/recipes/vagrant.rb
+++ cookbooks/cwb-server/recipes/vagrant.rb
@@ -1,32 +1,37 @@
 # Workaround for installing Vagrant plugin from source
 # Based on: https://github.com/sous-chefs/vagrant/blob/master/libraries/plugin.rb
 
-version = '2.3.1'
-butcher_path = "#{Chef::Config[:file_cache_path]}/cookbooks/cwb-server/files/default/vagrant-butcher-#{version}.gem"
-
 def username
   node['vagrant']['user']
 end
 
 def vagrant_home
-  user_home_dir = home_dir
-  ENV['VAGRANT_HOME'] || ::File.join(user_home_dir, '.vagrant.d') unless user_home_dir.nil?
+  ENV['VAGRANT_HOME'] || ::File.join(home_dir, '.vagrant.d') unless home_dir.nil?
 end
 
 def home_dir
-  return Dir.home unless username
-  # Unix only!
-  begin
-    Dir.home(username)
-  rescue ArgumentError
-    Chef::Log.warn('[recipes/vagrant] User not found: Cannot determine home directory for Vagrant!')
+  if username
+    "/home/#{username}"
+  else
+    Dir.home
   end
+end
+
+version = '2.3.1'
+butcher_file_name = "vagrant-butcher-#{version}.gem"
+butcher_path = File.join(home_dir, butcher_file_name)
+cookbook_file butcher_path do
+  source butcher_file_name
+  owner node['cwb-server']['user']
+  group node['cwb-server']['user']
+  mode '0755'
+  action :create
 end
 
 vagrant_home_var = vagrant_home
 plugin_exists_cmd = "vagrant plugin list | grep 'vagrant-butcher (#{version})'"
 execute 'install vagrant-butcher plugin' do
-  command "#{plugin_exists_cmd} || (vagrant plugin install #{butcher_path}; echo 'INSTALLED butcher'>/home/apps/test.log)"
+  command "#{plugin_exists_cmd} || vagrant plugin install #{butcher_path}"
   user node['vagrant']['user'] if node['vagrant']['user']
   environment('VAGRANT_HOME' => vagrant_home_var) if vagrant_home_var
 end
