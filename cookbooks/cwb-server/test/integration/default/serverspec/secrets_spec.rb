7d7a4fa972a0d6f6f15b0ccd0cb41cffd68e2ef0
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-12-15 14:11:11 +0100

    Update tests for new google and azure config

--- cookbooks/cwb-server/test/integration/default/serverspec/secrets_spec.rb
+++ cookbooks/cwb-server/test/integration/default/serverspec/secrets_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe 'cwb-server::secrets' do
   describe file('/home/apps/.ssh/cloud-benchmarking.pem') do
-    its(:content) { should match(/-----BEGIN RSA PRIVATE KEY-----/) }
+    its(:content) { should match(/-----BEGIN OPENSSH PRIVATE KEY-----/) }
+    it { should be_mode 600 }
+    it { should be_owned_by 'apps' }
+  end
+
+  describe file('/home/apps/.ssh/cloud-benchmarking.pem.pub') do
+    its(:content) { should match(/ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDSns2ln6lkzBopBV1MuxCM6+oba3OXmW7ihQ2Bjo8bvkMXIwbbk+4L89xW7X7mU4SPmFkk0WdRt1PxQIshahGUYEydbRfXUogcP6ohqyF0Zrxz5/uRfumF/T9OLK4sQPtGbh9NJPljHtccdkgXiCor7K/ZLIlHb2LS4FU2xqNSNQ== cwb@test.com/) }
     it { should be_mode 600 }
     it { should be_owned_by 'apps' }
   end
@@ -30,13 +36,12 @@ describe 'cwb-server::secrets' do
 
   # Providers
   providers_dir = '/home/apps/providers'
-  google_file = "#{providers_dir}/google/google-compute.pem"
-  azure_file = "#{providers_dir}/azure/azure.pem"
+  google_file = "#{providers_dir}/google/google.pem"
   describe file('/etc/systemd/system/cloud-workbench-job@.service') do
     its(:content) { should match(/Environment="AWS_ACCESS_KEY=my_aws_access_key"/) }
-    its(:content) { should match(/Environment="GOOGLE_API_KEY_NAME=google-compute"/) }
-    its(:content) { should match(/Environment="GOOGLE_API_KEY_PATH=#{google_file}"/) }
-    its(:content) { should match(/Environment="AZURE_MGMT_CERTIFICATE_PATH=#{azure_file}"/) }
+    its(:content) { should match(/Environment="GOOGLE_PROJECT_ID=my_google_project_id"/) }
+    its(:content) { should match(/Environment="GOOGLE_JSON_KEY_PATH=#{google_file}"/) }
+    its(:content) { should match(/Environment="AZURE_TENANT_ID=my_azure_tenant_id"/) }
     its(:content) { should match(%r{Environment="CHEF_SERVER_URL=https://33.33.33.10:443/organizations/chef"}) }
     its(:content) { should match(/Environment="CWB_SERVER_HOST=\d+\.\d+\.\d+.\d+"/) }
     # Contains Ruby PATH
@@ -44,13 +49,7 @@ describe 'cwb-server::secrets' do
   end
 
   describe file(google_file) do
-    its(:content) { should eq('my_google_compute_key') }
-    it { should be_mode 600 }
-    it { should be_owned_by 'apps' }
-  end
-
-  describe file(azure_file) do
-    its(:content) { should eq('my_azure_certificate_file_content') }
+    its(:content) { should eq('{"google": "my_google_secret"}') }
     it { should be_mode 600 }
     it { should be_owned_by 'apps' }
   end
