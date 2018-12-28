6f9021970e9e0f583bad848f56e470b2e65114ce
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-12-28 20:05:34 +0100

    Fix postgresql local mismatch
    Known issue: https://github.com/sous-chefs/postgresql
    See: https://github.com/sous-chefs/postgresql/issues/555
    target_locale = 'en_US.UTF-8'

--- cookbooks/cwb-server/recipes/database.rb
+++ cookbooks/cwb-server/recipes/database.rb
@@ -1,4 +1,7 @@
 db = node['cwb-server']['db']
+# Known issue: https://github.com/sous-chefs/postgresql
+# See: https://github.com/sous-chefs/postgresql/issues/555
+target_locale = 'en_US.UTF-8'
 
 # Docs: https://github.com/sous-chefs/postgresql
 postgresql_server_install 'Install PostgreSQL' do
@@ -7,6 +10,7 @@ postgresql_server_install 'Install PostgreSQL' do
 end
 
 postgresql_server_install 'Setup my PostgreSQL server' do
+  initdb_locale target_locale
   password db['postgres_password']
   port db['port']
   action :create
@@ -18,5 +22,6 @@ postgresql_user db['user'] do
 end
 
 postgresql_database db['name'] do
+  locale target_locale
   owner db['user']
 end
