8c712a2117ec2c2a5ef12f40baeff7ef8ffc4da1
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-01-03 11:21:36 +0100

    Add test for PostgreSQL DB locale

--- cookbooks/cwb-server/test/integration/default/serverspec/database_spec.rb
+++ cookbooks/cwb-server/test/integration/default/serverspec/database_spec.rb
@@ -7,4 +7,11 @@ describe 'cwb-server::database' do
   describe port(5432) do
     it { should be_listening.on('127.0.0.1').with('tcp') }
   end
+  psql_cmd = 'psql --command="\l"'
+  describe command("sudo su postgres -c '#{psql_cmd}'") do
+    # Encoding
+    its(:stdout) { should match(/UTF8/) }
+    # Locale
+    its(:stdout) { should match(/en_US\.UTF-8/) }
+  end
 end
