df2ef1d7b1edbd1e5dc617555a00b4d107fccf78
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-06-14 17:23:53 +0200

    Add berks-cookbooks

--- cookbooks/cwb-workstation/berks-cookbooks/cwb-workstation/metadata.rb
+++ cookbooks/cwb-workstation/berks-cookbooks/cwb-workstation/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joel.scheuner.dev@gmail.com'
 license          'Apache-2.0'
 description      'Installs/Configures cwb-workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 chef_version     '>= 14'
 supports         'ubuntu'
 
